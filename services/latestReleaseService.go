9391fcfa7bbe6cd9031dcee9e0bb150d7a04b88e
RuinedYourLife <debhi.quentin@gmail.com>
2024-02-11 06:39:59 +0000

    feat: more recent & update

--- services/latestReleaseService.go
+++ services/latestReleaseService.go
@@ -27,3 +27,26 @@ func (rs *LatestReleaseService) Create(name string, releaseDate time.Time, spoti
 	}
 	return nil
 }
+
+func (rs *LatestReleaseService) IsMoreRecent(artistID uint, latestRelease models.LatestRelease) (bool, error) {
+	var currentLatestRelease models.LatestRelease
+	result := db.DB.Where("artist_id = ?", artistID).First(&currentLatestRelease)
+	if result.Error != nil {
+		return false, result.Error
+	}
+	return currentLatestRelease.ReleaseDate.Before(latestRelease.ReleaseDate), nil
+}
+
+func (rs *LatestReleaseService) Update(artistID uint, latestRelease models.LatestRelease) error {
+	var currentLatestRelease models.LatestRelease
+	result := db.DB.Where("artist_id = ?", artistID).Updates(&currentLatestRelease)
+	if result.Error != nil {
+		return result.Error
+	}
+
+	currentLatestRelease.Name = latestRelease.Name
+	currentLatestRelease.ReleaseDate = latestRelease.ReleaseDate
+	currentLatestRelease.SpotifyID = latestRelease.SpotifyID
+
+	return db.DB.Save(&currentLatestRelease).Error
+}
