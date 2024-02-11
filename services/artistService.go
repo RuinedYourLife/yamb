5c3af202209feae89665bfa97ad9915aa7d25f7a
RuinedYourLife <debhi.quentin@gmail.com>
2024-02-11 03:16:28 +0000

    feat: return id for created artist

--- services/artistService.go
+++ services/artistService.go
@@ -2,7 +2,6 @@ package services
 
 import (
 	"errors"
-	"log"
 
 	"github.com/ruined.yamb/v1/db"
 	"github.com/ruined.yamb/v1/models"
@@ -15,14 +14,13 @@ func NewArtistService() *ArtistService {
 	return &ArtistService{}
 }
 
-func (as *ArtistService) Create(name, spotifyID string) error {
+func (as *ArtistService) Create(name, spotifyID string) (uint, error) {
 	artist := models.Artist{Name: name, SpotifyID: spotifyID}
 	result := db.DB.Create(&artist)
 	if result.Error != nil {
-		log.Printf("error creating artist: %v", result.Error)
-		return result.Error
+		return 0, result.Error
 	}
-	return nil
+	return artist.ID, nil
 }
 
 func (as *ArtistService) FindByName(name string) (*models.Artist, error) {
@@ -32,7 +30,6 @@ func (as *ArtistService) FindByName(name string) (*models.Artist, error) {
 		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
 			return nil, nil
 		}
-		log.Printf("error finding artist by name: %v", result.Error)
 		return nil, result.Error
 	}
 	return &artist, nil
