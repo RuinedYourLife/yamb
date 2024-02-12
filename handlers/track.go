f0a867731842319b81450c2b3931c313fd78a1c8
RuinedYourLife <debhi.quentin@gmail.com>
2024-02-12 06:13:09 +0000

    feat: embed replies

--- handlers/track.go
+++ handlers/track.go
@@ -10,65 +10,74 @@ import (
 
 func TrackCommandHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	spotifyURL := i.ApplicationCommandData().Options[0].StringValue()
+
 	spotifyService := services.NewSpotifyService()
 	spotifyID := spotifyService.ExtractSpotifyID(spotifyURL)
+	if spotifyID == "" || len(spotifyID) != 22 {
+		sendErrorReply(s, i, "Invalid Spotify URL provided")
+		return
+	}
 
 	artistDetails, err := spotifyService.FindArtistDetails(spotifyID)
 	if err != nil {
-		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionResponseData{
-				Content: "Could not find artist with this URL.",
-			},
-		})
+		sendErrorReply(s, i, "Could not find artist details for this URL")
 		return
 	}
 
 	latestRelease, err := spotifyService.FindArtistLatestRelease(spotifyID)
 	if err != nil {
-		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionResponseData{
-				Content: "Could not find latest release for this artist.",
-			},
-		})
+		sendErrorReply(s, i, "Could not find latest release for this artist")
 		return
 	}
 
 	artistService := services.NewArtistService()
 	artistID, err := artistService.Create(artistDetails.Name, artistDetails.SpotifyID)
 	if err != nil {
-		content := "Could not register artist."
+		content := "Could not register artist"
 		switch e := err.(type) {
 		case *yamberrors.AlreadyTrackedError:
 			content = e.Error()
 		}
 
-		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionResponseData{
-				Content: content,
-			},
-		})
+		sendErrorReply(s, i, content)
 		return
 	}
 
 	releaseService := services.NewLatestReleaseService()
 	err = releaseService.Create(latestRelease.Name, latestRelease.ReleaseDate, latestRelease.SpotifyID, artistID)
 	if err != nil {
-		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionResponseData{
-				Content: "Could not register latest release.",
-			},
-		})
+		sendErrorReply(s, i, "Could not register latest release for this artist")
 		return
 	}
 
 	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
 		Data: &discordgo.InteractionResponseData{
-			Content: fmt.Sprintf("Tracking new releases for %s", artistDetails.Name),
+			Embeds: []*discordgo.MessageEmbed{
+				{
+					Title:       artistDetails.Name,
+					URL:         fmt.Sprintf("https://open.spotify.com/artist/%s", artistDetails.SpotifyID),
+					Description: "is now being tracked",
+					Color:       0xD4AF91,
+					Thumbnail: &discordgo.MessageEmbedThumbnail{
+						URL: artistDetails.Images[0].URL,
+					},
+				},
+			},
+		},
+	})
+}
+
+func sendErrorReply(s *discordgo.Session, i *discordgo.InteractionCreate, message string) {
+	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Embeds: []*discordgo.MessageEmbed{
+				{
+					Description: message,
+					Color:       0xBD5773,
+				},
+			},
 		},
 	})
 }
